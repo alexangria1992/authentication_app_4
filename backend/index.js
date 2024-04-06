c234941a62e987ea46f80c55bf70889eb5ecf5bb
Alexandre Angrignon <alex040892@gmail.com>
2024-04-07 10:04:23 +1200

    Database Connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,31 @@
 import express from "express";
 import colors from "colors";
+import mysql from "mysql";
 
+//EXPRESS INITIALIZATION
 const app = express();
 
+//DB Connection
+const con = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "",
+  database: "nodejsdb",
+});
+
+con.connect(function (err) {
+  if (err) {
+    console.error("erro connecting: " + err.stack);
+    return;
+  }
+  console.log(colors.magenta("Connected to database"));
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// PORT
 const port = 3000;
 
 app.listen(port, () => {
