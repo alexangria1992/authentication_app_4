a644a06d788442189b17b41f01bc0a9ed47ef53f
Alexandre Angrignon <alex040892@gmail.com>
2024-04-07 12:54:17 +1200

    setup frontend and initial state in signup form

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Signup from "./components/signup";
+// import Login from "./components/login";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/login" element={<Login />} /> */}
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
